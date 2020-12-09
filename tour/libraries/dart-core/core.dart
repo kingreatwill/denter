f79bda01304f366c22f3b384d98e1a75275b1a2d
kingreatwill <350840291@qq.com>
2020-12-09 15:27:55 +0800

    dart

---
+++ tour/libraries/dart-core/core.dart
@@ -0,0 +1,6 @@
+void main() {
+  // https://dart.cn/guides/libraries/library-tour#dartcore---numbers-collections-strings-and-more
+  assert(int.parse('42') == 42);
+  assert(int.parse('0x42') == 66);
+  assert(double.parse('0.50') == 0.5);
+}
